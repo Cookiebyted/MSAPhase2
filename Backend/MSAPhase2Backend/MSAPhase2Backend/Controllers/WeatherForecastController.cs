4b9b7c81c543b79a1dfbf4ff9ee5f66b749ce817
Matt <myou129@aucklanduni.ac.nz>
2022-08-10 21:13:33 +1200

    intial backend files commit

---
+++ Backend/MSAPhase2Backend/MSAPhase2Backend/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MSAPhase2Backend.Controllers
+{
+    /// <summary>
+    /// This is the weather forecast controller that comes as a default from the project template when creating
+    /// a new c# Web API application.
+    /// </summary>
+    /// 
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// This endpoint takes no arguments, and gives you the weather forecast
+        /// </summary>
+        /// <returns>A JSON object describing the weather forecast</returns>
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
